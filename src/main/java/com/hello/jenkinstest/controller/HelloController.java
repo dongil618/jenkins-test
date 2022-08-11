5a32b902990d4a7a6ee4d77f0a81051b6aec7f52
dongil618 <rlaehddlf618@gmail.com>
2022-08-11 11:18:44 +0900

    chore : return 수정

--- src/main/java/com/hello/jenkinstest/controller/HelloController.java
+++ src/main/java/com/hello/jenkinstest/controller/HelloController.java
@@ -1,2 +1,13 @@
-package com.hello.jenkinstest.controller;public class HelloController {
+package com.hello.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello jenkins nice to meet you!";
+    }
 }
