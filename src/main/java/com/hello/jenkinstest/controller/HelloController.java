2ba3c6670f5bb5bb641975388525695e82259b36
dongil618 <rlaehddlf618@gmail.com>
2022-08-11 22:49:22 +0900

    배포중 push 테스트

--- src/main/java/com/hello/jenkinstest/controller/HelloController.java
+++ src/main/java/com/hello/jenkinstest/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "hello jenkins nice to meet you!!!!s!!";
+        return "hello jenkins nice to meet you!!!! test";
     }
 }
