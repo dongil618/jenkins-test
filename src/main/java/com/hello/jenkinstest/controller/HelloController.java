997c645e1c62fa5b169d90cd9ca4449c37b4968f
dongil618 <rlaehddlf618@gmail.com>
2022-08-11 11:51:50 +0900

    왜 안돼

--- src/main/java/com/hello/jenkinstest/controller/HelloController.java
+++ src/main/java/com/hello/jenkinstest/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "hello jenkins nice to meet you!";
+        return "hello jenkins nice to meet you!!!!!!!!!!";
     }
 }
