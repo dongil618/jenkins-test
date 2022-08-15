eb25820f1e1e9fb61a78a1ccb46f0a96164cf096
dongil618 <rlaehddlf618@gmail.com>
2022-08-15 21:38:12 +0900

    선진님 cpuboundapplication 폴더 파일 복사

--- src/main/java/com/hello/jenkinstest/controller/HelloController.java
+++ src/main/java/com/hello/jenkinstest/controller/HelloController.java
@@ -1,13 +1,36 @@
 package com.hello.jenkinstest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello(){
-        return "hello jenkins nice to meet you!!!! test + load balence12";
+    @RequestMapping("/hash/{input}")
+    public String getDigest(@PathVariable("input") String input) throws NoSuchAlgorithmException {
+        for (int i = 0; i < 100_000; i++) {
+            input = getMD5Digest(input);
+        }
+        return input;
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    private String getMD5Digest(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(input.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter
+                .printHexBinary(digest).toUpperCase();
+
+        return myHash;
     }
 }
