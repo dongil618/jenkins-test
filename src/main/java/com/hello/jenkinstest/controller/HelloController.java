97e39d5a12e1d510ee23174ca4fbb4b8b68e6f14
dongil618 <rlaehddlf618@gmail.com>
2022-08-15 21:09:39 +0900

    서버 껐다가 다시킴

--- src/main/java/com/hello/jenkinstest/controller/HelloController.java
+++ src/main/java/com/hello/jenkinstest/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "hello jenkins nice to meet you!!!! test + load balence1111";
+        return "hello jenkins nice to meet you!!!! test + load balence1";
     }
 }
